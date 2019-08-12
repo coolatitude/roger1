47fa6b756747eb5fd2943320dd6ea39336584f95
Pierre Vanderlynden <pvanderl@w-r11-p3.s19.be>
2019-08-12 17:12:47 +0200

    V3.sh

--- V3.sh
+++ V3.sh
@@ -1,12 +1,13 @@
+PAT="~/.roger/V3/"
 #network configuration
 apt remove ifupdown
-cp ./V3/99-disable-network-config.cfg /etc/cloud/cloud.cfg.d/
+cp ${PAT}99-disable-network-config.cfg /etc/cloud/cloud.cfg.d/
 mkdir ~/.roger/trash
 mv /etc/netplan/* ~/.roger/trash
-cp ./V3/01-network-card.yaml /etc/netplan/
+cp ${PAT}01-network-card.yaml /etc/netplan/
 netplan generate
 netplan apply
 
 #change ssh port
-echo ./V3/sshd_config >> /etc/ssh/sshd_config.cfg
+echo ${PAT}sshd_config >> /etc/ssh/sshd_config.cfg
 systemctl restart ssh
--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/coolatitude/roger1 ~/.roger
-sudo sh ~/.roger/V2.sh
-sudo sh ~/.roger/V3.sh
+sh ~/.roger/V2.sh
+sh ~/.roger/V3.sh
 
