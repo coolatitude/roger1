8e76e871ee2de19db6e6d1065deb9731b2c55b6c
Pierre Vanderlynden <pvanderl@w-r11-p3.s19.be>
2019-08-12 16:53:40 +0200

    V3.sh

--- V3.sh
+++ V3.sh
@@ -1,48 +1,12 @@
-non-root user:
-	do it at the installation
+#network configuration
+apt remove ifupdown
+cp ./V3/99-disable-network-config.cfg /etc/cloud/cloud.cfg.d/
+mkdir ~/.roger/trash
+mv /etc/netplan/* ~/.roger/trash
+cp ./V3/01-network-card.yaml /etc/netplan/
+netplan generate
+netplan apply
 
-use sudo:
-	default config
-
-Stop DHCP:
-	set a custom nat network on virtualbox
-	use this:
-		https://www.itzgeek.com/how-tos/linux/ubuntu-how-tos/netplan-how-to-configure-static-ip-address-in-ubuntu-18-04-using-netplan.html
-	192.10.0.56/30
-
-change ssh port:
-	change "PORT" line in /etc/ssh/sshd_config.cfg and add 23 // the default port is 22
-	set port forwarding on virtualbox
-
-set firewall:
-&&
-DOS protection:
-&&
-protection against scans:
-	used this config link http://lepepe.github.io/sysadmin/2016/01/19/ubuntu-server-ufw.html
-
-
-before:
-	echo "SHELL=/bin/bash\nMAILTO=root\n" >> /home/pvanderl/ex.cron
-
-script to update:
-	echo "apt -q -y upgrade >> /var/log/update_script.log" > /home/pvanderl/update.sh
-	echo "	0 4 * * 0		root	/home/pvanderl/update.sh
-			@reboot			root	/home/pvanderl/update.sh\n" >> ex.cron
-	crontab -u pvanderl ex.cron
-
-script to monitor changes:
-	echo "
-FILE="/etc/crontab"
-
-[[ -z `find $FILE -newermt "-24 hours"` ]]
-
-if [ $? -eq 0 ]
-then
-    exit 1
-else
-    mail -s "$FILE has been modfified." root
-fi" > /home/pvanderl/check.sh
-
-	echo "	0 0 * * *		root	/home/pvanderl/check.sh" >> ex.cron
-	crontab -u pvanderl ex.cron
+#change ssh port
+echo ./V3/sshd_config >> /etc/ssh/sshd_config.cfg
+systemctl restart ssh
